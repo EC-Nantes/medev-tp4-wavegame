7f06b1a9622b4a61220ee7eb3aa9575334ae0c8f
Bachir_Masroua <bachir.masroua2@gmail.com>
2025-12-08 16:42:38 +0100

    add testJoueur

---
+++ JeuDeDame/src/test/java/jeudedame/JoueurTest.java
@@ -0,0 +1,81 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package jeudedame;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author bachirmasroua
+ */
+public class JoueurTest {
+    
+    public JoueurTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getNom method, of class Joueur.
+     */
+    @Test
+    public void testGetNom() {
+        System.out.println("getNom");
+        Joueur instance = null;
+        String expResult = "";
+        String result = instance.getNom();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of isNoir method, of class Joueur.
+     */
+    @Test
+    public void testIsNoir() {
+        System.out.println("isNoir");
+        Joueur instance = null;
+        boolean expResult = false;
+        boolean result = instance.isNoir();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of toString method, of class Joueur.
+     */
+    @Test
+    public void testToString() {
+        System.out.println("toString");
+        Joueur instance = null;
+        String expResult = "";
+        String result = instance.toString();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
